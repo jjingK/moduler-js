cc75283af9305eadc3a9dd49d4e81b4b8ee8d168
kwonhaejin <hjkwon@rsupport.com>
2017-02-25 00:24:28 +0900

    Setting handlebars

--- server.js
+++ server.js
@@ -5,9 +5,32 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack.config';
 
+import hbs from 'hbs';
+
 const app = express();
 const port = 3030;
 const compiler = webpack(config);
+const dummy = {
+  lists: [
+    { name: 'Lee' },
+    { name: 'Kim' },
+    { name: 'Kang' },
+    { name: 'Kim' },
+    { name: 'Kwon' },
+    { name: 'Jeong' },
+    { name: 'Kim' },
+    { name: 'Eric' },
+    { name: 'Tomas'},
+    { name: 'Anna'},
+    { name: 'Robert'}
+  ]
+}
+
+hbs.registerPartials(__dirname + '/views');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
 
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
 app.use(webpackDevMiddleware(compiler, {
@@ -18,7 +41,7 @@ app.use(webpackDevMiddleware(compiler, {
 app.use(webpackHotMiddleware(compiler));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('index', dummy);
 });
 
 app.listen(port, () => {
